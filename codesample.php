c9077707b74363f53d32790429f5588a1ccba32e
Dan Tereschenko <d.tereschenko@paymantix.com>
2015-12-03 11:05:52 +0300

    DEV-5075: code review fix

--- codesample.php
+++ codesample.php
@@ -5,13 +5,14 @@ const URL = 'https://terminal-sandbox.ecommpay.com/';
 // пример PHP кода, формирующего url запроса на терминал с выводом его в html button
 
 //Подключение библиотеки генерации подписи https://github.com/com-devcookies/signgen
-require_once(dirname(__FILE__) . '/devcookies/SignatureGenerator.php'); 
+require_once(dirname(__FILE__) . '/devcookies/SignatureGenerator.php');
 use devcookies\SignatureGenerator;
 
 $salt = 'askdfjlaIAuhahsk9891823912'; //Salt ("соль") сайта
 $signer = new SignatureGenerator($salt); // Инициализация генератора подписи
 
-$params = array( //Массив с передаваемыми параметрами
+//Массив с передаваемыми параметрами, за исключением неподписываемых
+$params = array(
 	'site_id' => '1',
 	'amount' => '1000',
 	'currency' => 'RUB',
@@ -19,13 +20,11 @@ $params = array( //Массив с передаваемыми параметра
 //	'success_url' => 'http://www.example.com/success',
 //	'decline_url' => 'http://www.example.com/decline',
 //	'callback_method' => 2,
-
 );
 $params['signature'] = $signer->assemble($params); //Добавление подписи
 
-//Если у вас настроены какие-либо из перечисленных ниже неподписываемых параметров,
-//их добавлять здесь
-
+//Если у вас в настройках сайта ВЫКЛЮЧЕНЫ какие-либо из перечисленных ниже параметров,
+//их добавлять здесь. Уточните этот момент у технической поддержки
 $unsigned = array(
 //	'email' => 'sherlock@example.com',
 //	'billing_country' => 'GB',
@@ -36,17 +35,17 @@ $unsigned = array(
 //	'billing_phone' => '+7-100-200-30-40'
 );
 
-$params = array_merge($params, $unsigned);
+$result = array_merge($params, $unsigned);
 
 //Вариант с GET-запросом
-$url = URL .'?'. http_build_query($params);
+$url = URL .'?'. http_build_query($result);
 echo "<button onclick=\"location.href='{$url}';\">Отправить</button>";
 
-//Вариант с POST-запросом, 
+//Вариант с POST-запросом,
 //если количество передаваемых данных >2kb, GET-запрос может не работать в IE
 ?>
 <form method="post" action="<?=URL;?>">
-<?php foreach ($params as $name => $value): ?>
+<?php foreach ($result as $name => $value): ?>
 	<input type="hidden" name="<?=$name; ?>" value="<?=$value;?>">
 <?php endforeach;?>
 	<button type="submit">Отправить</button>
