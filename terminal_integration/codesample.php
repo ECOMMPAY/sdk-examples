d980d4ad13b81d176dd6c72032181486b5f8a38f
Dan Tereschenko <d.tereschenko@paymantix.com>
2015-12-04 14:57:18 +0300

    DEV-5075: code review fixes

---
+++ terminal_integration/codesample.php
@@ -0,0 +1,49 @@
+<?php
+
+const URL = 'https://terminal-sandbox.ipsp.tld/'; // URL, на который шлём запрос
+const SALT = 'askdfjlaIAuhahsk9891823912'; // Salt ("соль") сайта
+
+// пример PHP кода, формирующего url запроса на терминал с выводом его в html button
+
+// Подключение библиотеки генерации подписи (находится в папке /signgen)
+require_once '/path/to/devcookies/SignatureGenerator.php';
+use devcookies\SignatureGenerator;
+
+$signer = new SignatureGenerator(SALT); // Инициализация генератора подписи
+
+// Массив с передаваемыми параметрами, за исключением неподписываемых
+$params = array(
+	'site_id' => '1',
+	'amount' => '1000',
+	'currency' => 'RUB',
+	'external_id' => 'TEST_12345677',
+);
+$params['signature'] = $signer->assemble($params); //Добавление подписи
+
+// Если у вас в настройках сайта ВЫКЛЮЧЕНЫ какие-либо из перечисленных ниже параметров,
+// их добавлять здесь. Уточните этот момент у технической поддержки
+$unsigned = array(
+//	'email' => 'sherlock@example.com',
+//	'billing_country' => 'GB',
+//	'billing_region' => 'GB-LND',
+//	'billing_city' => 'London',
+//	'billing_address' => '221B Baker street',
+//	'billing_postal' => 'NW1NW1',
+//	'billing_phone' => '+7-100-200-30-40'
+);
+
+$result = array_merge($params, $unsigned);
+
+// Вариант с GET-запросом
+$url = URL .'?'. http_build_query($result);
+echo "<button onclick=\"location.href='{$url}';\">Отправить</button>";
+
+// Вариант с POST-запросом,
+// если количество передаваемых данных >2kb, GET-запрос может не работать в IE
+?>
+<form method="post" action="<?=URL;?>">
+<?php foreach ($result as $name => $value): ?>
+	<input type="hidden" name="<?=$name; ?>" value="<?=$value;?>">
+<?php endforeach;?>
+	<button type="submit">Отправить</button>
+</form>
