ad316bb3a9fef0e39ff615998e2f570d7de232cd
Dan Tereschenko <d.tereschenko@paymantix.com>
2015-12-08 11:33:58 +0300

    DEV-5075: unsigned params removed

--- terminal_integration/codesample.php
+++ terminal_integration/codesample.php
@@ -21,31 +21,17 @@ $params = array(
 );
 $params['signature'] = $signer->assemble($params); // Добавление подписи
 
-// Если у вас в настройках сайта исключены из подписи какие-либо из перечисленных ниже параметров,
-// их добавлять здесь. Уточните этот момент у технической поддержки
-$unsigned = array(
-//	'email' => 'sherlock@example.com',
-//	'billing_country' => 'GB',
-//	'billing_region' => 'GB-LND',
-//	'billing_city' => 'London',
-//	'billing_address' => '221B Baker street',
-//	'billing_postal' => 'NW1NW1',
-//	'billing_phone' => '4952003040'
-);
-
-$result = array_merge($params, $unsigned);
-
 // Выберите один из двух приведенных ниже вариантов
 
 // Вариант с GET-запросом
-$url = URL .'?'. http_build_query($result);
+$url = URL .'?'. http_build_query($params);
 echo "<button onclick=\"location.href='{$url}';\">Отправить</button>";
 
 // Вариант с POST-запросом,
 // если количество передаваемых данных >2kb, GET-запрос может не работать в IE
 ?>
 <form method="post" action="<?=URL;?>">
-<?php foreach ($result as $name => $value): ?>
+<?php foreach ($params as $name => $value): ?>
 	<input type="hidden" name="<?=$name; ?>" value="<?=$value;?>">
 <?php endforeach;?>
 	<button type="submit">Отправить</button>
